9e739fb2eb22fb571d345a4c8e7cb2eb0f7edc26
akshaykrishnan23 <110891490+akshaykrishnan23@users.noreply.github.com>
2022-11-08 19:26:10 +0530

    Add files via upload

---
+++ python16.py
@@ -0,0 +1,10 @@
+#let x be a tuple and this is a Python program to get the 4th element and 4th element from last of a tuple
+#
+#
+#
+x=("a","b","d","c","1","2","3","e")
+print(x)
+i=x[3]
+print (i)
+k=x[-4]
+print(k)
