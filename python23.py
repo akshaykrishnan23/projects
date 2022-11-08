16f0c251be85a496aeb1ef47300a04b4afd9eeea
akshaykrishnan23 <110891490+akshaykrishnan23@users.noreply.github.com>
2022-11-09 01:20:51 +0530

    Update python23.py

--- python23.py
+++ python23.py
@@ -1,3 +1,4 @@
+#RANDOM PASSWORD GENERATOR
 import random
 digits=['0','1','2','3','4','5','6','7','8','9']
 lowercase=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h','i', 'j', 'k', 'm', 'n', 'o', 'p', 'q','r', 's', 't', 'u', 'v', 'w', 'x', 'y','z']
