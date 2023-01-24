5adcc654744bf7472fa8613361f76971d39f9de3
Akshay Krishnan R <110891490+akshaykrishnan23@users.noreply.github.com>
2023-01-24 09:17:42 +0530

    Add files via upload

---
+++ src/components/Listcontainer.js
@@ -0,0 +1,13 @@
+import Listitem from "./Listitem";
+function Listcontainer(props) {
+    return <div className="col-md-12">
+        {props.todos.map((todo)=>(
+            <Listitem 
+                key={todo.id}
+                todoitem={todo}
+                toggleMarked={props.toggleMark}
+                deleteItem={props.deleteItem}/>
+        ))}     
+    </div>;
+}
+export default Listcontainer;
